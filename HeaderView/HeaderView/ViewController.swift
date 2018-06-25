ba37ce1647af08df6e7d23993c1858fef9420196
Chris <cbecom@cardinalsolutions.com>
2018-06-25 11:47:06 -0400

    If applied, this commit adds the initial project files.

---
+++ HeaderView/HeaderView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeaderView
+//
+//  Created by Becom, Chris on 6/25/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
